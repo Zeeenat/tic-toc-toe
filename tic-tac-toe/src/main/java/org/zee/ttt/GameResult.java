e0811fcb256e49190e7834f8d56d1f1b832aec4e
Zeeenat <102461288+Zeeenat@users.noreply.github.com>
2022-05-06 13:01:14 +0500

    Add files via upload

---
+++ tic-tac-toe/src/main/java/org/zee/ttt/GameResult.java
@@ -0,0 +1,5 @@
+package org.zee.ttt;
+
+public enum GameResult {
+    CROSSES_WON, NAUGHTS_WON, DRAW, GAME_CONTINUES;
+}
---
+++ tic-tac-toe/src/main/java/org/zee/ttt/Main.java
@@ -0,0 +1,7 @@
+package org.zee.ttt;
+
+public class Main {
+    public static void main(String[] args) {
+        new GameEngine().play();
+    }
+}
