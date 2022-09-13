656c894189395443af2ec8d39b6f2cb8e9495a3d
ZiynatI <zisomurodova@gmail.com>
2022-09-13 20:46:53 +0500

    Player created

--- src/main/java/org/zee/ttt/Player.java
+++ src/main/java/org/zee/ttt/Player.java
@@ -7,15 +7,31 @@ public class Player {
         this.value = value;
     }
 
-    public static Value[][] makeMove(Value[][] grid, Value value) {
+    public static int makeMove(Value[][] grid, Value value, int moves) {
+        if (value.equals(Value.CROSS)) {
+            if (moves >= 5) {
+
+            }
+        } else if (value.equals(Value.NAUGHT)) {
+
+        }
         for (int row = 0; row < grid.length; row++) {
             for (int cell = 0; cell < grid[row].length; cell++) {
                 if (grid[row][cell].equals(Value.EMPTY)) {
                     grid[row][cell] = value;
-                    return grid;
+                    return row * 10 + cell;
                 }
             }
         }
-        return grid;
+        return 0;
+    }
+
+    public static int winningSituation(Value[][] grid, Value value) {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid.length; j++) {
+                Value newValue = grid[i][j];
+            }
+        }
+        return -1;
     }
 }
