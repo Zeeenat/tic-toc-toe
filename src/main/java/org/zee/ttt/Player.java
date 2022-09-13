3e93f3355ce639ea1c6651f29c5919eb68755fd3
ZiynatI <zisomurodova@gmail.com>
2022-09-13 22:29:07 +0500

    Player updated

--- src/main/java/org/zee/ttt/Player.java
+++ src/main/java/org/zee/ttt/Player.java
@@ -10,10 +10,16 @@ public class Player {
     public static int makeMove(Value[][] grid, Value value, int moves) {
         if (value.equals(Value.CROSS)) {
             if (moves >= 5) {
-
+                if (winningSituation(grid, value) != -1) {
+                    return winningSituation(grid, value);
+                }
             }
         } else if (value.equals(Value.NAUGHT)) {
-
+            if (moves >= 6) {
+                if (winningSituation(grid, value) != -1) {
+                    return winningSituation(grid, value);
+                }
+            }
         }
         for (int row = 0; row < grid.length; row++) {
             for (int cell = 0; cell < grid[row].length; cell++) {
@@ -27,9 +33,66 @@ public class Player {
     }
 
     public static int winningSituation(Value[][] grid, Value value) {
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid.length; j++) {
-                Value newValue = grid[i][j];
+
+        {
+            if ((grid[0][1] == grid[0][2] && grid[0][1] == value) ||
+                    (grid[1][0] == grid[2][0] && grid[2][0] == value) ||
+                    (grid[1][1] == grid[2][2] && grid[2][2] == value)) {
+                return 0;
+            }
+            {
+                if ((grid[0][0] == grid[0][2] && grid[0][0] == value) ||
+                        (grid[1][1] == grid[2][1] && grid[1][1] == value)) {
+                    return 1;
+                }
+            }
+            {
+                if ((grid[0][0] == grid[0][1] && grid[0][0] == value) ||
+                        (grid[1][2] == grid[2][2] && grid[1][2] == value) ||
+                        (grid[1][1] == grid[2][0] && grid[1][1] == value)) {
+                    return 2;
+                }
+            }
+            {
+                if ((grid[0][0] == grid[2][0] && grid[0][0] == value) ||
+                        (grid[1][1] == grid[1][2] && grid[1][1] == value)) {
+                    return 10;
+                }
+            }
+            {
+                if ((grid[0][0] == grid[2][2] && grid[0][0] == value) ||
+                        (grid[0][1] == grid[2][1] && grid[0][1] == value) ||
+                        (grid[0][2] == grid[2][0] && grid[0][2] == value) ||
+                        (grid[1][0] == grid[1][2] && grid[1][0] == value)) {
+                    return 11;
+                }
+
+            }
+            {
+                if ((grid[0][2] == grid[2][2] && grid[0][2] == value) ||
+                        (grid[1][0] == grid[1][1] && grid[1][0] == value)) {
+                    return 12;
+                }
+            }
+            {
+                if ((grid[0][0] == grid[1][0] && grid[0][0] == value) ||
+                        (grid[2][1] == grid[2][2] && grid[2][1] == value) ||
+                        (grid[0][2] == grid[1][1] && grid[1][1] == value)) {
+                    return 20;
+                }
+            }
+            {
+                if ((grid[0][1] == grid[1][1] && grid[0][1] == value) ||
+                        (grid[2][0] == grid[2][2] && grid[2][0] == value)) {
+                    return 21;
+                }
+            }
+            {
+                if ((grid[0][0] == grid[1][1] && grid[1][1] == value) ||
+                        (grid[0][2] == grid[1][2] && grid[0][2] == value) ||
+                        (grid[2][0] == grid[2][1] && grid[2][0] == value)) {
+                    return 22;
+                }
             }
         }
         return -1;
