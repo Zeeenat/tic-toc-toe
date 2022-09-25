c685da55b7f221f17c457e6169e0e578bd1a752c
Alex Abdugafarov <frozenspider@users.noreply.github.com>
2022-09-25 19:25:52 +0600

    Implemented VirtualPlayer strategy, tweaked Cell class

--- src/main/java/org/zee/ttt/Cell.java
+++ src/main/java/org/zee/ttt/Cell.java
@@ -1,5 +1,6 @@
 package org.zee.ttt;
 
+import java.util.Objects;
 
 public class Cell {
     public final int row;
@@ -8,5 +9,24 @@ public class Cell {
     public Cell(int row, int column) {
         this.row = row;
         this.column = column;
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(column, row);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      if (this == obj) return true;
+      if (obj == null) return false;
+      if (getClass() != obj.getClass()) return false;
+      Cell other = (Cell) obj;
+      return column == other.column && row == other.row;
+    }
+
+    @Override
+    public String toString() {
+      return "(" + row + "; " + column + ")";
     }
 }
